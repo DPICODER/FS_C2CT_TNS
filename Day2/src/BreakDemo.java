826af8ac298fe8d77081a2f2d5850692e1506500
P.SaiVarun <107747742+DPICODER@users.noreply.github.com>
2023-10-16 12:02:32 +0530

    Day-2

---
+++ Day2/src/BreakDemo.java
@@ -0,0 +1,19 @@
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<10;i++)
+		{
+			if(i==5)
+				continue;
+
+			System.out.println(i);
+		
+		
+		}
+	}
+		//break: terminates the operation
+		//continue: skips the current iteration
+
+}
