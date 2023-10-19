bc98cc8867e036061c1483ddf3f3d43e47fc0505
P.SaiVarun <107747742+DPICODER@users.noreply.github.com>
2023-10-19 11:16:22 +0530

    Interface introduction and its Understanding

---
+++ Interfaces_Java/src/com/cg/InterfaceExample/Circle.java
@@ -0,0 +1,14 @@
+package com.cg.InterfaceExample;
+
+public class Circle implements Shape{
+
+	public void input() {
+		System.out.println("Interface Example");
+	}
+	
+	public void radius() {
+		System.out.println("Radius ");
+	}
+
+	
+}
