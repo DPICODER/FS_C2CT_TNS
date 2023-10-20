99d7fd16ea7bca7a3e4d46418c4f5f8bfb6d8e0d
P.SaiVarun <107747742+DPICODER@users.noreply.github.com>
2023-10-20 12:16:34 +0530

    Array-Demo-Push-attempt-1

---
+++ ArrayExamples/src/com/cg/arrayexamples/Notes.java
@@ -0,0 +1,17 @@
+package com.cg.arrayexamples;
+
+public class Notes {
+//	used for storing group of inputs of similar data type (Homogeneous)
+//	it stores in a consecutive manner 12345
+// Manipulation is done using index numbers 
+//	Fixed Length
+//	Array is a predefined class -> where object / instance will be created 
+//	 creating a ARRAY
+//		datatype Variablename [] = new datatype[size];
+	
+//		example :-
+				int a[] = {1,2,3,4,5};
+//Static and Dynamic
+// Types of Arrays - Single and Multi-dimentional Array
+// [] -> is called "SUBSCRIPT'S"
+}
